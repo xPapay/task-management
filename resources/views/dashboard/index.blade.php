2de5a039f8d3124941f5fe83adbef0294352f87b
xPapay <papay.lukas@gmail.com>
2019-02-19 17:50:58 +0100

    Style titles on dashboard page

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
-    <h2 class="m-top-l">My tasks</h2>
+    <h2 class="m-top-l carbon fs-2x font-thinner">My tasks</h2>
     <tasks></tasks>
-    <h2 class="m-top-l">Supervised tasks</h2>
+    <h2 class="m-top-l carbon fs-2x font-thinner">Supervised tasks</h2>
     <tasks type="supervising"></tasks>
     <new-task :users="{{ $users }}"></new-task>
 @endsection
