e11d289af95ecdae1ba11072e6988e07e9bededc
xPapay <papay.lukas@gmail.com>
2019-02-25 19:40:37 +0100

    Swap demo database before authentication

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -73,10 +73,10 @@ class Kernel extends HttpKernel
     protected $middlewarePriority = [
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\CheckForDemoMode::class,
         \App\Http\Middleware\Authenticate::class,
         \Illuminate\Session\Middleware\AuthenticateSession::class,
         \Illuminate\Routing\Middleware\SubstituteBindings::class,
         \Illuminate\Auth\Middleware\Authorize::class,
-        \App\Http\Middleware\CheckForDemoMode::class,
     ];
 }
